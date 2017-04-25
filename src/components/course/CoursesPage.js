09a09050699d6871a15f90e2a52d3b346a6c2428
SOLAR\pedro.fernandes <pedro.fernandes@savills.com>
2017-04-25 11:35:24 +0100

    Final Init App

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,27 @@
+import React, { Component, PropTypes } from 'react'
+
+const propTypes = {};
+
+const defaultProps = {};
+
+class CoursesPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h1>
+            </div>
+        );
+    };
+};
+
+CoursesPage.propTypes = propTypes;
+
+CoursesPage.defaultProps = defaultProps;
+
+export default CoursesPage;
