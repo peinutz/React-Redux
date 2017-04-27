f58499a953a52ed393e0294ed54f574f8da9ce80
Pedro Fernandes <Pedro Fernandes>
2017-04-27 22:26:42 +0100

    git fix

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,4 +1,7 @@
 import React, { Component, PropTypes } from 'react'
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux'
+import * as courseActions from '../../actions/courseActions';
 
 const propTypes = {};
 
@@ -8,20 +11,60 @@ class CoursesPage extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            course: { title:""}
         }
+
+        this.onClickSave = this.onClickSave.bind(this);
+        this.onTitleChange = this.onTitleChange.bind(this);
+    }
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>
+    }
+    onTitleChange(event) {
+        const course = this.state.course
+        course.title = event.target.value
+        this.setState({course:course})
+    }
+
+    onClickSave() {
+        this.props.actions.createCourse(this.state.course);
     }
 
     render() {
         return (
             <div>
-                <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h1>
+            <h1>Courses</h1>
+            {this.props.courses.map(this.courseRow)}
+            <h2>Add Course</h2>
+            <input type="text" 
+            onChange={this.onTitleChange}
+            value={this.state.course.title} />
+
+            <input type="submit"
+                value="Save"
+                onClick={this.onClickSave} />
             </div>
         );
-    };
-};
+    }
+}
 
-CoursesPage.propTypes = propTypes;
+CoursesPage.propTypes = {    
+    actions:PropTypes.object.isRequired,
+    courses:PropTypes.array.isRequired
+};
 
 CoursesPage.defaultProps = defaultProps;
 
-export default CoursesPage;
+function mapStateToProps(state, ownProps) {
+    return {
+        courses:state.courses
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(courseActions, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,9 @@
+export default function courseReducer(state = [{title: "init"}], action) {
+    switch(action.type) {
+        case 'CREATE_COURSE' :
+            return [...state,
+            Object.assign({}, action.course)]
+        default:
+            return state;
+    }
+}
